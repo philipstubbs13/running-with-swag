80da11fafc2c200f0fb1ea1e5aaf531eb6194075
philipstubbs13 <philipstubbs13@gmail.com>
2018-07-22 15:22:24 -0500

    more work on home page

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,8 @@
 // import React
 import React, { Component } from 'react';
 // import UI components from PrimeReact.
-import {Button} from 'primereact/button';
-// import image
-import Athlete from '../../images/athlete.jpg';
+import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
 // import css
 import './Home.css';
 
@@ -11,14 +10,17 @@ class Home extends Component {
   render() {
     return (
       <div className="home-container">
-        <header class="showcase">
+        <header className="showcase">
           <h1>Running with Swag</h1>
           <p>Your stop for running events, blog posts, reviews, tips, and more</p>
-          <Button label="LEARN MORE" className="btn"/>
+          <Button label="LEARN MORE" className="btn" />
         </header>
-        <div class="sidebar">
-          Placeholder text
-        </div>
+        <Card title="Welcome" className="card welcome">
+        </Card>
+        <Card title="Looking for developers" className="card developer">
+          <p>Are you a web developer interested in running and want to help build this site?</p>
+          <p>For more information on how you can contribute, send an email to philipstubbs13@gmail.com.</p>
+        </Card>
       </div>
     );
   }
