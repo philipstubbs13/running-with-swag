e622b3d2f74588df6c2b9ea87904e55420b6d60a
philipstubbs13 <philipstubbs13@gmail.com>
2018-07-24 05:28:32 -0500

    cleanup

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,52 +8,70 @@ import { Card } from 'primereact/card';
 // import css
 import './Home.css';
 
+// Define the Home page component.
 class Home extends Component {
   render() {
     return (
-      <div className="home-container">     
+      // Main container for home page.
+      <div className="home-container">
+        {/* Top header on homae page. */}
         <div className="home-header">
           <h1>Running with Swag</h1>
           <p>Your stop for running events, blog posts, reviews, tips, and more</p>
         </div>
         <header className="showcase">
+          {/* Background image is defined in the Home.css file. */}
         </header>
+        {/* Welcome to my site card */}
         <Card title="Welcome" className="card welcome">
           <Link to="/about">
-           <Button label="LEARN MORE" className="btn" />
+            <Button label="LEARN MORE" className="btn" />
           </Link>
         </Card>
+        {/* Looking for developers to work on site card */}
         <Card title="Looking for developers" className="card developer">
           <p>Are you a web developer interested in running and want to help build this site?</p>
-          <p>For more information on how you can contribute, send an email to philipstubbs13@gmail.com.</p>
+          <p>For more information on how you can contribute,
+            send an email to philipstubbs13@gmail.com.
+          </p>
         </Card>
+        {/* Find races card */}
         <Card className="card events">
           <i className="fas fa-calendar-alt fa-3x" />
           <h2>Events</h2>
           <Link to="/events">
             <Button label="See events" className="btn" />
           </Link>
-          <p>Look for upcoming racing events in your area. 5K, 10K, 15K, half marathon, full marathon, and more.</p>
+          <p>Look for upcoming racing events in your area. 5K, 10K, 15K,
+            half marathon, full marathon, and more.
+          </p>
         </Card>
+        {/* Check out my blog card. */}
         <Card className="card blog">
           <i className="fa fa-fw fa-pen-alt fa-3x" />
           <h2>Blog</h2>
           <Link to="/blog">
             <Button label="Read blog" className="btn" />
           </Link>
-          <p>Read some swaggin' articles about running. Here, you will find race stories, running tips, and more.</p>
+          <p>Read some swaggin' articles about running.
+            Here, you will find race stories, running tips, and more.
+          </p>
         </Card>
+        {/* Check out my product reviews card */}
         <Card className="card reviews">
           <i className="fas fa-star fa-3x" />
           <h2>Reviews</h2>
           <Link to="/reviews">
             <Button label="Check reviews" className="btn" />
           </Link>
-          <p>Check out reviews of some of the races I ran as well as products I have bought to help me train.</p>
+          <p>Check out reviews of some of the races I ran as
+            well as products I have bought to help me train.
+          </p>
         </Card>
       </div>
     );
   }
 }
 
+// export the Home component so that the app can render the Home page.
 export default Home;
