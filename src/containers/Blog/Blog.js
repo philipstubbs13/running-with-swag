51b8f2c149204b8757c60d54190ba97ab3116362
philipstubbs13 <philipstubbs13@gmail.com>
2018-07-27 07:31:25 -0500

    more work on blog page

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,12 +1,21 @@
 // Possible Blog sites to use: Tumblr.
 // import React
 import React, { Component } from 'react';
+// import css
+import './Blog.css';
 
 class Blog extends Component {
   render() {
     return (
-      <div>
-          Blog
+      <div className="blog-container">
+        <div className="main">
+          <h1 className="page-title">Blog</h1>
+          <div className="blog-header-image">
+          </div>
+        </div>
+        <div className="sidebar">
+          <h1>Sidebar</h1>
+        </div>
       </div>
     );
   }
