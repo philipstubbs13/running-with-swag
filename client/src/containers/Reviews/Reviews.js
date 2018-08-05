63e7529c0f522663478b32479b82d4d7e3244eaa
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-05 13:48:46 -0500

    imported data from json. image not rendering yet

--- client/src/containers/Reviews/Reviews.js
+++ client/src/containers/Reviews/Reviews.js
@@ -5,24 +5,21 @@ import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 // import css
 import './Reviews.css';
-import bottle from '../../images/bottle.jpg';
+// import reviews
+import reviews from './reviews.json';
 
 class Reviews extends Component {
   render() {
     return (
       <div className="reviews-container">
         <h1 className="reviews-page-header">Reviews</h1>
-        <div className="review-card">
-          <h2 className="review-title">Neptune Bottle</h2>
-          <img src={bottle} alt="neptune bottle" className="review-image" />
-          <div className="review-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</div>
-        </div>
-
-        <div className="review-card">
-          <h2 className="review-title">Neptune Bottle 2</h2>
-          <img src={bottle} alt="neptune bottle" className="review-image" />
-          <div className="review-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</div>
-        </div>
+        {reviews.map(review => (
+          <div className="review-card" key={review.id}>
+            <h2 className="review-title">{review.title}</h2>
+            <img src={`${review.image}`} alt={review.title} className="review-image" />
+            <div className="review-text">{review.review}</div>
+          </div>
+        ))};
       </div>
     );
   }
