c1176f73da41c05aea31b6c49fd6f0233742d257
philipstubbs13 <philipstubbs13@gmail.com>
2018-08-05 14:32:35 -0500

    refining layout of reviews page

--- client/src/containers/Reviews/Reviews.js
+++ client/src/containers/Reviews/Reviews.js
@@ -11,16 +11,28 @@ import reviews from './reviews.json';
 class Reviews extends Component {
   render() {
     return (
-      <div className="reviews-container">
-        <h1 className="reviews-page-header">Reviews</h1>
-        {reviews.map(review => (
-          <div className="review-card" key={review.id}>
-            <h2 className="review-title">{review.title}</h2>
-            <img src={`${review.image}`} alt={review.title} className="review-image" />
-            <div className="review-text">{review.review}</div>
+        <div className="reviews-container">
+          <div className="reviews-header-container">
+            <div className="reviews-page-header">
+              <h1>Reviews</h1>
+            </div>
+            <div className="reviews-info">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+            </div>
           </div>
-        ))};
-      </div>
+          <div className="reviews-content-container">
+            {reviews.map(review => (
+              <div className="review-card" key={review.id}>
+                <h2 className="review-title">{review.title}</h2>
+                <img src={review.image} className="review-image" />
+                <div className="review-text">{review.review}</div>
+                <a href={review.link} target="_blank" rel="noopener noreferrer">
+                  <Button label={review.link_text} className="btn review-btn" />
+                </a>
+              </div>
+            ))};
+          </div>
+        </div>
     );
   }
 }
